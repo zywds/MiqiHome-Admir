5c360a0fd70d92eca7a8449bb34ea6fd7ca7ddce
luoliyi <mopln510@qq.com>
2019-02-25 11:31:36 +0800

    出租方式

--- src/components/page/RentalMode.vue
+++ src/components/page/RentalMode.vue
@@ -4,17 +4,20 @@
         <marquee style="color: cadetblue">出租方式管理</marquee>
         <el-table
                 :data="bedData"
-                border
-                style="width: 100%">
+                style="width: 100%"
+                :default-sort = "{prop: 'date', order: 'descending'}"
+        >
             <el-table-column
                     prop="rmId"
                     label="编号"
-                    width="150">
+                    sortable
+                    width="350">
             </el-table-column>
             <el-table-column
                     prop="rmName"
                     label="类型"
-                    width="120">
+                    sortable
+                    width="350">
             </el-table-column>
             <el-table-column
                     fixed="right"
@@ -25,34 +28,35 @@
                     <el-button type="text" size="small" @click="editFirstRows(scope.row)">编辑</el-button>
                 </template>
             </el-table-column>
-
         </el-table>
         <!--
             新增
         -->
-        <el-button type="primary">新增类型</el-button>
+        <el-button type="primary" @click="dialogFormVisible = true">新增类型</el-button>
         <!--
             弹出层
         -->
-        <el-dialog title="编辑信息" :visible.sync="dialogFormVisible">
+        <el-dialog title="信息操作" :visible.sync="dialogFormVisible">
             <el-form >
                 <el-form-item label="编号" :label-width="formLabelWidth">
                    <!-- <el-input  v-model="this.rmId" autocomplete="off"></el-input>-->
                     <el-input v-model="form.rmId" placeholder="请输入内容"></el-input>
                 </el-form-item>
                 <el-form-item label="类型" :label-width="formLabelWidth">
-                    <el-input v-model="form.rmName" autocomplete=""></el-input>
+                    <el-input v-model="form.rmName" autocomplete="" placeholder="请输入内容"></el-input>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="editSucc()">确 定</el-button>
+                <el-button type="primary" @click="addOne()">新 增</el-button>
+                <el-button type="primary" @click="editSucc()">编 辑</el-button>
             </div>
         </el-dialog>
     </div>
 </template>
 
 <script>
+    import  qs from 'qs'
     export default {
         name: "RentalMode",
         methods: {
@@ -65,57 +69,73 @@
                 this.form.rmId=row.rmId;
                 this.form.rmName=row.rmName;
             },
+
+            /*
+            * 编辑 put请求
+            * */
             editSucc(){
-                console.log(this.form.rmId);
-                console.log(this.form.rmName);
-                this.axios.put("http://localhost:8080/houseBed/update_rentalMode",{
-                    params: {
-                        rmName: "ddd",
-                        rmId: 1
+                var that = this;
+
+                var readyData= qs.stringify({
+                    rmName: this.form.rmName,
+                    rmId: this.form.rmId
+                });
+                this.axios.put("http://localhost:8080/houseBed/update_rentalMode?"+readyData).then(function (res) {
+                   console.log(res);
+                    if(res.data.code===1){
+                        that.$message({
+                            message: '成功',
+                            type: 'success'
+                        });
+                        that.axios.get("http://localhost:8080/houseBed/select_rentalMode",{}).then(function (res) {
+                            that.bedData=res.data.data;
+                            console.log(that.bedData)
+                        })
                     }
-                }).then(function (res) {
-                   console.log("返回值："+res);
                 });
-                this.dialogFormVisible = false;
-                const h = this.$createElement;
-                this.$notify({
-                    title: '编辑成功',
-                   // message: h('i', { style: 'color: teal'}, '这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案')
+                this.dialogFormVisible=false;
+            },
+
+            /*
+            * 新增 post请求
+            * */
+            addOne(){
+                var that = this;
+                if(this.form.rmName==""){
+                    this.$message({
+                        message: '空值错误',
+                        type: 'warning'
+                    });
+                    return false;
+                }
+                this.axios.post("http://localhost:8080/houseBed/insert_rentalMode",qs.stringify({
+                    rmName: this.form.rmName
+                })).then(function (res) {
+                    console.log(res.data.code);
+                    if(res.data.code==1){
+                        that.$message({
+                            message: '成功',
+                            type: 'success'
+                        });
+                        that.axios.get("http://localhost:8080/houseBed/select_rentalMode",{}).then(function (res) {
+                            that.bedData=res.data.data;
+                            console.log(that.bedData)
+                        })
+                    }
                 });
+
+                this.dialogFormVisible=false;
+
             }
         },
         data() {
             return {
                 bedData:[],
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }],
+                tableData: [],
                 dialogFormVisible: false,
                 form: {
                     rmId:"",
-                    rmName:"",
-                   /* name: '',
-                    region: '',
-                    date1: '',
-                    date2: '',
-                    delivery: false,
-                    type: [],
-                    resource: '',
-                    desc: ''*/
+                    rmName:""
                 },
                 formLabelWidth: '120px'
             };
