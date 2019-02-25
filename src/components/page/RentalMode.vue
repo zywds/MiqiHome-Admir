732e07560ad0effa75b93e2fe462f264ac285cb8
luoliyi <mopln510@qq.com>
2019-02-25 09:29:01 +0800

    各模块管理

--- src/components/page/RentalMode.vue
+++ src/components/page/RentalMode.vue
@@ -25,74 +25,12 @@
                     <el-button type="text" size="small" @click="editFirstRows(scope.row)">编辑</el-button>
                 </template>
             </el-table-column>
-           <!-- <el-table-column
-                    prop="province"
-                    label="省份"
-                    width="120">
-            </el-table-column>
-            <el-table-column
-                    prop="city"
-                    label="市区"
-                    width="120">
-            </el-table-column>
-            <el-table-column
-                    prop="address"
-                    label="地址"
-                    width="300">
-            </el-table-column>
-            <el-table-column
-                    prop="zip"
-                    label="邮编"
-                    width="120">
-            </el-table-column>-->
 
         </el-table>
-        <br/>
-        <!--<h3 style="font-family: 微软雅黑">房屋类型管理</h3>
-        <el-table
-                :data="tableData"
-                border
-                style="width: 100%">
-            <el-table-column
-                    prop="date"
-                    label="日期"
-                    width="150">
-            </el-table-column>
-            <el-table-column
-                    prop="name"
-                    label="姓名"
-                    width="120">
-            </el-table-column>
-            <el-table-column
-                    prop="province"
-                    label="省份"
-                    width="120">
-            </el-table-column>
-            <el-table-column
-                    prop="city"
-                    label="市区"
-                    width="120">
-            </el-table-column>
-            <el-table-column
-                    prop="address"
-                    label="地址"
-                    width="300">
-            </el-table-column>
-            <el-table-column
-                    prop="zip"
-                    label="邮编"
-                    width="120">
-            </el-table-column>
-            <el-table-column
-                    fixed="right"
-                    label="操作"
-                    width="100">
-                <template slot-scope="scope">
-                    <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-                    <el-button type="text" size="small" @click="editRows(scope.row)">编辑</el-button>
-                </template>
-            </el-table-column>
-        </el-table>-->
+        <!--
+            新增
+        -->
+        <el-button type="primary">新增类型</el-button>
         <!--
             弹出层
         -->
@@ -128,12 +66,12 @@
                 this.form.rmName=row.rmName;
             },
             editSucc(){
-
+                console.log(this.form.rmId);
                 console.log(this.form.rmName);
                 this.axios.put("http://localhost:8080/houseBed/update_rentalMode",{
                     params: {
-                        hcName: this.form.rmName,
-                        hcId: this.form.rmId
+                        rmName: this.form.rmName,
+                        rmId: this.form.rmId
                     }
                 }).then(function (res) {
                    console.log("返回值："+res);
